644e80bde8ac8c4d0a0aeb663845026b6076e4c7
Orlov Dmitriy <orlovdmi.igor@gmail.com>
2024-03-13 15:20:40 +0200

    chore(shadcn): add shadcn button component

--- components/layout/Sidebar.vue
+++ components/layout/Sidebar.vue
@@ -5,8 +5,9 @@
 <template>
   <aside class="px-5 py-8 bg-sidebar h-full relative w-full">
     <NuxtLink to="/" class="mb-10 block">
-      <NuxtImg src="/logo.png" alt="logo" width="100px"></NuxtImg>
+      <NuxtImg src="/logo.png" alt="logo" width="100px" class="mx-auto" />
     </NuxtLink>
+    <UiButton>Send</UiButton>
   </aside>
 </template>
 
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,7 +6,7 @@ export default defineNuxtConfig({
     'shadcn-nuxt',
   ],
   'shadcn': {
-    prefix: '',
+    prefix: 'Ui',
     componentDir: './components/ui'
   }
 })
