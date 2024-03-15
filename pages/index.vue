7249bbc3f7748aa61f4423c41442dad0b9ca59d1
Orlov Dmitriy <orlovdmi.igor@gmail.com>
2024-03-15 12:31:48 +0200

    feat(kanban): add kanban and deals cards

--- pages/index.vue
+++ pages/index.vue
@@ -7,6 +7,13 @@ useHead({
 <template>
   <div class="p-10">
     <h1 class="text-2xl font-bold mb-10">Nuxt 3 CRM</h1>
+    <div>
+      <UiCard class="mb-3" draggable="true">
+        <UiCardHeader role="button">Card name</UiCardHeader>
+        <UiCardContent>Company</UiCardContent>
+        <UiCardFooter>Footer</UiCardFooter>
+      </UiCard>
+    </div>
   </div>
 </template>
 
